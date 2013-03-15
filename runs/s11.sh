1eef3d6c66747aba0525540af655d80a31bc502e
mosser <mosser@i3s.unice.fr>
2013-03-15 16:52:48 +0100

    s11

---
+++ runs/s11.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+## Loading DSL functions
+source ./functions.sh
+
+## describing what's expected this week
+function handle_project_repository() # $1 = GRP, $2 = $PROJECT_NAME, $3 = URL
+{
+    # Short
+    REMOTE=$3; D=$1-$2
+    KEY=`echo $3 | cut -d ';' -f 3 | cut -d '/' -f 5 | tr '[A-Z]' '[a-z]'`
+
+    # Check script
+    clone_repository   $REMOTE $D
+    checkout_git_tag   $D "week11"
+    exists_file        $D .gitignore
+    exists_pattern     $D '*.java'     "source files"
+    not_exists_pattern $D '*.class'    "compiled classes"
+    not_exists_pattern $D '*~'         "temporary files"
+    not_exists_pattern $D '.classpath' "Eclipse classpath meta-data"
+    not_exists_pattern $D '.project'   "Eclipse project meta-data"
+    not_exists_pattern $D '.settings'  "Eclipse settings"
+    exists_file        $D pom.xml
+    exists_directory   $D src/main/java/fr/unice/polytech/tse/$KEY 
+    run_maven          $D install
+}
+
+main
+
+
+
